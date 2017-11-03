3c21affdae2e9a9f947d87829e0af70ff9813b8b
Jeremy <hurleyjer@yahoo.com>
2017-11-02 22:20:54 -0400

    add strategy and rules components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameboardComponent } from './gameboard/gameboard.component';
+import { LandingComponent } from './landing/landing.component';
+import { RulesComponent } from './rules/rules.component';
+import { StrategyComponent } from './strategy/strategy.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component:  },
-  {path: 'play', component: GameboardComponent }
+  {path: '', pathMatch: 'full', component: LandingComponent },
+  {path: 'play', component: GameboardComponent },
+  {path: 'rules', component: RulesComponent },
+  {path: 'strategy', component: StrategyComponent }
 ];
 
 @NgModule({
